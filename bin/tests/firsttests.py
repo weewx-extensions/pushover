97da4777e18d60e6a40fb0750d3457dcd05614d3
Rich Bell <bellrichm@gmail.com>
2025-02-04 10:08:12 -0500

    More test setup

--- bin/tests/firsttests.py
+++ bin/tests/firsttests.py
@@ -1,5 +1,6 @@
 # pylint: disable=wrong-import-order
 # pylint: disable=missing-docstring
+# pylint: disable=invalid-name
 
 import unittest
 import mock
@@ -13,12 +14,23 @@ CONFIG_DICT = \
 """
 """
 
-class Tests(unittest.TestCase):
-    def tests_one(self):
+class TestObservationMissing(unittest.TestCase):
+    def tests_observation_missing_at_startup(self):
         print("one")
         mock_engine = mock.Mock()
         config = configobj.ConfigObj(io.StringIO(CONFIG_DICT))
+
+        observation_detail = {'last_sent_timestamp': 0,
+                              'wait_time': 0,
+                              'counter': 0,
+                              'count': 0,}
+
         SUT = Pushover(mock_engine, config)
+        msg = SUT.check_missing_value('outTemp8',
+                                      'outTemp8',
+                                      'label for outTemp8',
+                                      observation_detail)
+        print(msg)
 
         print("done")
 
@@ -26,8 +38,7 @@ if __name__ == '__main__':
     print("start")
 
     test_suite = unittest.TestSuite()
-    test_suite.addTest(Tests('tests_one'))
-    #test_suite.addTest(Tests('tests_two'))
+    test_suite.addTest(TestObservationMissing('tests_observation_missing_at_startup'))
     unittest.TextTestRunner().run(test_suite)
 
     #unittest.main(exit=False)
