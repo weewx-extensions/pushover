db175a81e4c2f04741981eeef0cd0c3d8ad516a1
Rich Bell <bellrichm@gmail.com>
2025-02-10 16:42:56 -0500

    Send a notification when back within the Max threshold

--- bin/user/tests/test_pushover.py
+++ bin/user/tests/test_pushover.py
@@ -346,10 +346,10 @@ class TestObservationEqualCheck(unittest.TestCase):
                                       record_value)
 
 
-        self.assertEqual(msg, f"{name}{label} value {record_value} is not equal to {value}.\n")
+        self.assertEqual(msg, f"{name}{label} value {record_value} is Not Equal to {value}.\n")
 
 class TestObservationMaxCheck(unittest.TestCase):
-    def test_observation_not_greater(self):
+    def test_observation_not_greater_no_notification(self):
         mock_engine = mock.Mock()
 
         binding = 'archive'
@@ -368,12 +368,50 @@ class TestObservationMaxCheck(unittest.TestCase):
 
         SUT = Pushover(mock_engine, config)
 
+        SUT.archive_observations[observation]['equal']['threshold_passed'] = {}
+        SUT.archive_observations[observation]['equal']['threshold_passed']['timestamp'] = \
+            time.time()
+        SUT.archive_observations[observation]['equal']['threshold_passed']['notification_count'] = 0
+
         msg = SUT.check_max_value(name,
                                      label,
                                       SUT.archive_observations[observation]['equal'],
                                       value)
 
         self.assertEqual(msg, "")
+
+    def test_observation_not_greater_with_notification(self):
+        mock_engine = mock.Mock()
+        now = time.time()
+
+        binding = 'archive'
+        observation = random_string()
+        label = f' {random_string()}'
+        name = observation
+        value = 99 # ToDo: make random int
+
+        config_dict = setup_config_dict(binding,
+                                        observation,
+                                        'equal',
+                                        label=label,
+                                        name=name,
+                                        value=value)
+        config = configobj.ConfigObj(config_dict)
+
+        SUT = Pushover(mock_engine, config)
+
+        SUT.archive_observations[observation]['equal']['threshold_passed'] = {}
+        SUT.archive_observations[observation]['equal']['threshold_passed']['timestamp'] = now
+        SUT.archive_observations[observation]['equal']['threshold_passed']['notification_count'] = 1
+
+        msg = SUT.check_max_value(name,
+                                     label,
+                                      SUT.archive_observations[observation]['equal'],
+                                      value)
+
+        self.assertEqual(msg,
+                         (f"{name}{label} over Max threshold at {timestamp_to_string(now)} "
+                          f"is within threshold after 0 with value {value}.\n"))
 
     def test_observation_greater_no_notification(self):
         mock_engine = mock.Mock()
