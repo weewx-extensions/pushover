42fcdf4458ea18903ac1015ad0afcb4aedb91b6e
Rich Bell <bellrichm@gmail.com>
2025-12-21 08:18:46 -0500

    simple test of http post

--- bin/user/tests/unit/test_pushover.py
+++ bin/user/tests/unit/test_pushover.py
@@ -11,14 +11,15 @@ import unittest
 import mock
 
 import configobj
-import json
 from collections import namedtuple
 
 from user.notify import Logger
 from user.pushover import PushOver
 
 class TestTest(unittest.IsolatedAsyncioTestCase):
-    async def test_test(self):
+    # This is a bit silly test, but it is a good template for testing HTTP Post
+    # ToDo: change call_count = 1 to called_once_with
+    async def test_error_sending_notification(self):
         mock_logger = mock.Mock(spec=Logger)
 
         config_dict = {}
@@ -26,7 +27,7 @@ class TestTest(unittest.IsolatedAsyncioTestCase):
 
         SUT = PushOver(mock_logger, config)
 
-        foobar = {
+        msg_data_dict = {
             'threshold_type': 'equal',
             'type': 'outside',
             'date_time': 1,
@@ -36,20 +37,25 @@ class TestTest(unittest.IsolatedAsyncioTestCase):
             'current_value': 102,
             'notifications_sent': 201,
         }
-        msg_data = namedtuple('FooBar', foobar.keys())(**foobar)
+        msg_data = namedtuple('MsgData', msg_data_dict.keys())(**msg_data_dict)
 
         mock_response = mock.Mock(name='mock_response')
         mock_response.code = 400
-        response_body = json.dumps({'errors': ['bar01', 'bar02']})
-        mock_response.read.return_value = response_body.encode()
 
-        with mock.patch('http.client.HTTPSConnection') as mock_connection:
-            mock_connection_instance = mock_connection.return_value
-            mock_connection_instance.getresponse.return_value = mock_response
+        with mock.patch('user.pushover.json') as mock_json:
+            with mock.patch('user.pushover.time'):
+                with mock.patch('http.client.HTTPSConnection') as mock_connection:
+                    mock_json.loads.return_value = {'errors': ['Error One', 'Error Two']}
+                    mock_connection_instance = mock_connection.return_value
+                    mock_connection_instance.getresponse.return_value = mock_response
 
-            await SUT.send_notification(msg_data)
+                    result = await SUT.send_notification(msg_data)
 
-        print("done")
+                    self.assertFalse(result)
+                    self.assertEqual(mock_connection.call_count, 1)
+                    self.assertEqual(mock_connection_instance.request.call_count, 1)
+                    self.assertEqual(mock_connection_instance.getresponse.call_count, 1)
+                    self.assertEqual(mock_response.read.call_count, 1)
 
 if __name__ == '__main__':
     # test_suite = unittest.TestSuite()
